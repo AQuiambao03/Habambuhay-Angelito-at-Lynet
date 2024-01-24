e94d5a643582c95127eab6dfaa716888c2616c6d
Angelito Quiambao <angelito.quiambao@tuitt.com>
2024-01-24 20:08:21 +0800

    Final invitation

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Add click event listener to trigger the door swing animation
-    document.querySelector('.wrapper').addEventListener('click', function () {
-        document.getElementById('left-door').style.transform = 'rotateY(-140deg)';
-        document.getElementById('right-door').style.transform = 'rotateY(140deg)';
-    });
-});
+function startBackgroundMusic() {
+  var audio = document.getElementById("backgroundMusic");
+  if (audio.paused) {
+    audio.play();
+  }
+}
